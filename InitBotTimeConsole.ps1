89041402b97de8c3af5b47ffc1a4ee2a5d44507f
炫东 陈 <chenxuandong@outlook.com>
2019-08-02 13:49:57 +0800

    add vm init script

---
+++ InitBotTimeConsole.ps1
@@ -0,0 +1,19 @@
+﻿Param
+(
+    [Parameter(Mandatory=$true)]
+    [String] $Endpoint
+)
+
+$apiEndpoint = "http://${Endpoint}:8080";
+$imageEndpoint = "http://${Endpoint}:8081";
+
+systemctl start mysqld
+systemctl start redis
+
+"nohup java -jar /encoo/imageServer.jar --port=8081 --host=${imageEndpoint} --env=privatedeploy &" | Out-File /encoo/startImageServer.sh -Force
+sh /encoo/startImageServer.sh
+
+sh /encoo/start.sh start
+
+(gci /encoo/frontend/index*js | gc).Replace("AP1P1ACEHOLDER", $apiEndpoint) | Out-File (gci /encoo/frontend/index*js).FullName -Force
+systemctl start nginx
