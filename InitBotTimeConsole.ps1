913d404eb26c1ba588bb1d8117353784fba1ea0f
炫东 陈 <chenxuandong@outlook.com>
2019-08-21 04:21:09 +0800

    update all

--- InitBotTimeConsole.ps1
+++ InitBotTimeConsole.ps1
@@ -10,7 +10,7 @@ $imageEndpoint = "http://${Endpoint}:8081";
 systemctl start mysqld
 systemctl start redis
 
-"nohup java -jar /encoo/imageServer.jar --port=8081 --host=${imageEndpoint} --env=privatedeploy &" | Out-File /encoo/startImageServer.sh -Force
+"nohup java -jar /encoo/imageServer.jar --port=8081 --host=${imageEndpoint} --path=/encoo/fileserver &" | Out-File /encoo/startImageServer.sh -Force
 sh /encoo/startImageServer.sh
 
 sh /encoo/start.sh start
