5289c08607746c12b45fe1cc74a56949dfc7c6e7
炫东 陈 <chenxuandong@outlook.com>
2019-08-27 04:02:49 +0800

    pass license

--- InitBotTimeConsole.ps1
+++ InitBotTimeConsole.ps1
@@ -2,12 +2,23 @@
 (
     [Parameter(Mandatory=$true)]
     [String] $Endpoint
+
+	[Parameter(Mandatory=$true)]
+	[String] $License
 )
 
 $apiEndpoint = "http://${Endpoint}:8080";
 $imageEndpoint = "http://${Endpoint}:8081";
 
 systemctl start mysqld
+
+if (-not [string]::IsNullOrEmpty($License))
+{
+	Start-Sleep -Seconds 10
+	"update consoledb.sys_config set config_value = '${License}' where config_id = 125;" | out-File /encoo/setLicense.sql -Force
+	mysql -h localhost -u consoleuser -p'Encoo123!@#' -e 'source /encoo/setLicense.sql'
+}
+
 systemctl start redis
 
 "nohup java -jar /encoo/imageServer.jar --port=8081 --host=${imageEndpoint} --path=/encoo/fileserver &" | Out-File /encoo/startImageServer.sh -Force
